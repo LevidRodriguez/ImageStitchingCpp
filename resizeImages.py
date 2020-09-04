8f0749f2af1f96bfac1035917bf94005df5c1cf3
Levid Rodriguez <levid.rodriguez@gmail.com>
2020-09-04 12:20:47 -0500

    update

--- resizeImages.py
+++ resizeImages.py
@@ -15,7 +15,9 @@ def resizeImages(image_dir, key_frame, output_dir):
     dir_list = filter(lambda x: x != key_frame, dir_list)
     print (dir_list)
     for image in dir_list:
-        img = cv2.resize(cv2.imread(image), (1280, 720))
+        # img = cv2.resize(cv2.imread(image), (1280, 720))
+        # img = cv2.resize(cv2.imread(image), (640, 480))
+        img = cv2.resize(cv2.imread(image), (320, 240))
         print (image.replace(image_dir, output_dir))
         image = image.replace(image_dir, output_dir)
         cv2.imwrite(image, img)
